bb6718b5543f8e5496009ef333143e87f180af1b
Aliaksei Urbanski <aliaksei.urbanski@itechart-group.com>
2017-09-08 19:54:12 +0300

    Fix compatibility with Django Grappelli

--- src/dal/static/admin/js/jquery.init.js
+++ src/dal/static/admin/js/jquery.init.js
@@ -1,8 +1,11 @@
+if (jQuery === undefined && grp !== undefined)
+    var jQuery = grp.jQuery;
+
 var django = django || {};
-django.jQuery = jQuery
+django.jQuery = jQuery;
 
 var yl = yl || {};
-yl.jQuery = django.jQuery
+yl.jQuery = django.jQuery;
 
 // this file also prevents django.jQuery.noConflict
 // there's got to be a better way ...
